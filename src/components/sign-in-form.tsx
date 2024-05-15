d087749ed4fd24b326a9bd738d8876432354097b
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-05-14 22:26:46 -0300

    feat: editing the form with formik

---
+++ src/components/sign-in-form.tsx
@@ -0,0 +1,121 @@
+import { useFormik } from 'formik'
+import { redirect } from 'react-router-dom'
+import { toast } from 'sonner'
+
+import { api } from '../lib/axios'
+
+export function SignInForm() {
+  async function handleSignInForm() {
+    try {
+      const response = await api.post('login/', values)
+
+      // TODO: Save token in local storage and then it should be able to login
+      localStorage.setItem('token', response.data.token) // Save token in local storage
+
+      console.log(response.data)
+
+      redirect('/profile')
+    } catch (error) {
+      toast.error('Não foi possível realizar login. Tente novamente.')
+
+      console.log(error)
+    }
+  }
+
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: () => handleSignInForm(),
+  })
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-full flex-col space-y-7 px-6"
+    >
+      <div className="flex w-full flex-col space-y-2">
+        <label htmlFor="email">Email </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={handleChange}
+          value={values.email}
+          className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
+        />
+      </div>
+
+      <div className="flex w-full flex-col space-y-2">
+        <label htmlFor="email">Password </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          value={values.password}
+          className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="h-14 rounded-lg bg-primary-blue text-zinc-50 hover:bg-primary-blue/90"
+      >
+        Sign In
+      </button>
+    </form>
+  )
+
+  //   function handleSubmit(values: SignInFormProps) {
+  //     // try {
+  //     //   const response = await api.post('/login/', values)
+  //     //   console.log(response.data)
+  //     // } catch (error) {
+  //     //   console.log('Deu merda...')
+  //     // }
+  //     alert(JSON.stringify(values, null, 2))
+  //   }
+
+  //   return (
+  //     <div className="w-full">
+  //       <Formik
+  //         initialValues={{ email: '', password: '' }}
+  //         validationSchema={SignInSchema}
+  //         onSubmit={handleSubmit}
+  //       >
+  //         <Form>
+  //           <div className="flex w-full flex-col space-y-7 px-6">
+  //             <div className="flex w-full flex-col space-y-2">
+  //               <label htmlFor="email">E-mail</label>
+  //               <Field
+  //                 name="email"
+  //                 className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
+  //                 type="email"
+  //                 placeholder="@gmail.com"
+  //               />
+  //             </div>
+
+  //             <div className="flex w-full flex-col space-y-2">
+  //               <label htmlFor="password">Password</label>
+  //               <Field
+  //                 id="password"
+  //                 className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
+  //                 type="password"
+  //                 placeholder="*****************"
+  //               />
+  //             </div>
+
+  //             <button
+  //               type="submit"
+  //               className="h-14 rounded-lg bg-primary-blue text-zinc-50 hover:bg-primary-blue/90"
+  //             >
+  //               Sign In
+  //             </button>
+  //           </div>
+  //         </Form>
+  //       </Formik>
+  // </div>
+  //   )
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from '../../components/card'
+import { SignInForm } from '../../components/sign-in-form'
 
 export function LoginPage() {
   return (
@@ -9,34 +10,7 @@ export function LoginPage() {
             b2b<span className="text-primary-yellow">it</span>
           </h1>
 
-          <form className="flex w-full flex-col space-y-7 px-6">
-            <section className="flex w-full flex-col space-y-2">
-              <label htmlFor="email">E-mail</label>
-              <input
-                id="email"
-                className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
-                type="text"
-                placeholder="@gmail.com"
-              />
-            </section>
-
-            <section className="flex w-full flex-col space-y-2">
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                className="h-14 rounded-lg bg-zinc-100 p-5 focus:border-primary-blue focus:outline-none focus:ring-1 focus:ring-primary-blue"
-                type="password"
-                placeholder="*****************"
-              />
-            </section>
-
-            <button
-              type="submit"
-              className="h-14 rounded-lg bg-primary-blue text-zinc-50 hover:bg-primary-blue/90"
-            >
-              Sign In
-            </button>
-          </form>
+          <SignInForm />
         </Card>
       </main>
     </>
