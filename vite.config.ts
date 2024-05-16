a0b775ffb983d983a1bf51e13ae8656e4ee5b427
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-05-16 14:27:25 -0300

    test: setting up tests enviroment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+  },
+} as UserConfig & { test: InlineConfig })
